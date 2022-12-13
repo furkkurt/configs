2480b23b3fe7ef408732933d04609d84b0bf0201
Furkan Kurt <69459954+furkkurt@users.noreply.github.com>
2022-12-13 17:23:25 +0300

    Add files via upload

---
+++ qutebrowser/greasemonkey/darculized-reddit.com.css.js
@@ -0,0 +1,163 @@
+// ==UserScript==
+// @name    Userstyle (darculized-reddit.com.css)
+// @include    *old.reddit.com*
+// ==/UserScript==
+GM_addStyle(`* {
+  border-color: #909396 !important;
+}
+a {
+  color: #8ad !important;
+}
+a:visited {
+  color: #909396 !important;
+}
+body {
+  background-color: #2e2e2e !important;
+  color: #909396 !important;
+}
+html {
+  background-color: #2e2e2e !important;
+}
+input,
+textarea {
+  background-color: #2e2e2e !important;
+  color: #909396 !important;
+}
+blockquote,
+pre {
+  background-color: #2e2e2e !important;
+  color: #909396 !important;
+}
+#header {
+  background-color: #777;
+}
+#sr-header-area {
+  background-color: #2e2e2e !important;
+}
+#sr-header-area .srdrop .selected {
+  color: #909396 !important;
+}
+#sr-header-area #sr-more-link {
+  background-color: #2e2e2e !important;
+}
+.link.promotedlink.promoted {
+  background-color: #2e2e2e !important;
+}
+.link .usertext .md {
+  background-color: #2e2e2e !important;
+}
+.linkinfo {
+  background-color: #2e2e2e !important;
+}
+.linklisting .odd {
+  background-color: #2e2e2e !important;
+}
+.thing .title {
+  color: #DEDEDE !important;
+}
+div.md {
+  color: #909396 !important;
+}
+body.comment {
+  background-color: #2e2e2e !important;
+}
+.commentarea > .usertext {
+  background-color: #2e2e2e !important;
+}
+.commentarea .menuarea {
+  background-color: #2e2e2e !important;
+}
+.comments-page .panestack-title {
+  background-color: #2e2e2e !important;
+}
+.listing-chooser {
+  background-color: #2e2e2e !important;
+}
+.listing-chooser .intro {
+  background-color: #393939 !important;
+  color: #a6aaab !important;
+}
+.listing-chooser .grippy {
+  background-color: #2e2e2e !important;
+}
+.listing-chooser .grippy:after {
+  border-right: none !important;
+}
+.listing-chooser li {
+  background-color: #393939 !important;
+}
+.listing-chooser li.selected {
+  background-color: #393939 !important;
+  box-shadow: none;
+}
+.listing-chooser li a {
+  color: #8ad !important;
+}
+.listing-chooser li a .description {
+  color: #a6aaab !important;
+}
+.titlebox {
+  background-color: #2e2e2e !important;
+}
+.titlebox form.toggle {
+  background-color: #2e2e2e !important;
+  color: #909396 !important;
+}
+.side .usertext-body .md {
+  background-color: #2e2e2e !important;
+}
+.side {
+  background-color: #2e2e2e !important;
+}
+.sidebox .spacer {
+  background-color: #2e2e2e !important;
+  color: #a6aaab !important;
+}
+.tabmenu li a {
+  background-color: #2e2e2e !important;
+}
+#header-bottom-right {
+  background-color: #2e2e2e !important;
+}
+.morelink .nub {
+  background-color: #2e2e2e !important;
+}
+.comment {
+    background-color: #222222 !important;
+}
+.sidecontentbox .content {
+    background-color: #222222 !important;
+}
+.link {
+    background-color: #222222 !important;
+}
+#sr-header-area .redesign-beta-optin {
+  visibility: hidden;
+  width: 0px;
+}
+.premium-banner__logo {
+  visibility: hidden;
+}
+.premium-banner__title {
+  visibility: hidden;
+}
+.premium-banner__button {
+  visibility: hidden;
+}
+.element.style {
+  visibility: hidden;
+}
+.premium-banner::before {
+  visibility: hidden;
+}
+.premium-banner {
+  visibility: hidden;
+  height: 0px;
+}
+.message {
+  background-color: #2e2e2e;
+}
+.roundfield {
+  background-color: #2e2e2e;
+}
+`)
